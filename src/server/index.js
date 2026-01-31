8cd7e930eed527c30c847a864d3b908064f94415
RishabhRawat2003 <rawatrishabh953@gmail.com>
2026-01-31 15:46:31 +0530

    added interval health check for 10 minutes

--- src/server/index.js
+++ src/server/index.js
@@ -115,14 +115,14 @@ connectDB().then(() => {
       process.exit(1);
     }
     console.log(`Core API is running on port: ${serverConfig.PORT}`);
-    // setInterval(async () => {
-    //   try {
-    //     const response = await axios.get('https://nexmentorbackend.onrender.com/health')
-    //     console.log(response.data)
-    //   } catch (error) {
-    //     console.error('Health check failed:', error)
-    //   }
-    // }, 10 * 60 * 1000)
+    setInterval(async () => {
+      try {
+        const response = await axios.get('https://neev-backend.onrender.com/health')
+        console.log(response.data)
+      } catch (error) {
+        console.error('Health check failed:', error)
+      }
+    }, 10 * 60 * 1000)
   });
 }).catch((error) => {
   console.error('Startup failed:', error);
